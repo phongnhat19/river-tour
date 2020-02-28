ae3182f170c9ab67588fe90528179a36d5d6ed5a
Nhat Nguyen <phongnhat19@gmail.com>
2020-02-28 11:10:53 +0700

    init

---
+++ src/renderer/components/App.tsx
@@ -0,0 +1,11 @@
+import {hot} from "react-hot-loader/root";
+import React from "react";
+
+export default hot((): JSX.Element => 
+    (
+        <div>
+          <h1>
+            Your Electron App
+          </h1>
+        </div>
+    ));
